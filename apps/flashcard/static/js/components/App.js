91ffa01f04c58fc2c0201681508f77c7eefa19b3
Matthew <mchen146@ucsc.edu>
2023-05-26 21:57:58 -0700

    simplified template

--- apps/flashcard/static/js/components/App.js
+++ apps/flashcard/static/js/components/App.js
@@ -1,13 +1,11 @@
 import test from './test.js';
 
-export default await (async () => {
-    return {
-        components: {
-            test
-        },
-        data() {
-            return {}
-        },
-        template: '<div>Hello <test></test></div>'
-    }
-})() 
+export default await {
+    components: {
+        test
+    },
+    data() {
+        return {}
+    },
+    template: `<div>Hello <test></test></div>`
+}
